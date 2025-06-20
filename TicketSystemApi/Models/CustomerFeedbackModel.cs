0edfc21c93128230c57bc925b8bd161613a354e2
Prakash Nishad <48586833+prakashmiracle2707@users.noreply.github.com>
2025-06-20 11:41:47 +0530

    customer feedback api

---
+++ TicketSystemApi/Models/CustomerFeedbackModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TicketSystemApi.Models
+{
+    public class CustomerFeedbackModel
+    {
+        public string CaseId { get; set; }
+        public int Rating { get; set; }
+        public string Comment { get; set; }
+    }
+}
